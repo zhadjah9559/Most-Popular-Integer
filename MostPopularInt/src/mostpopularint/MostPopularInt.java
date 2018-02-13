2e104e7071505a53c760172c9bdb4d35443fb022
zhadjah9559 <zhadjah@gmail.com>
2018-02-12 21:28:14 -0500

    Initial version

--- MostPopularInt/src/mostpopularint/MostPopularInt.java
+++ MostPopularInt/src/mostpopularint/MostPopularInt.java
@@ -1,35 +1,56 @@
-package MostPopularInt;
+/*==============================================================================
+Zachary Hadjah                      February 9th, 2018
+Junior Year                         Side Project
+
+Description:
+This program takes an array populated with integers and displays what the most
+popular element inside the array is. 
+
+==============================================================================*/
+package mostpopularint;
+
+import java.util.Arrays;
 
 public class MostPopularInt 
 {
-
+/**
+ * 
+ * @param args 
+ */
     public static void main(String[] args) 
     {
         int[] array = {16,16,42,42,42,16,17,95,55,95,95,16,42,66,155,16};
+        System.out.println("The given array is: " + Arrays.toString(array));        
+        System.out.println("The most frequent integer value is: " + mostPopular(array));
+    }
+    
+    /**
+     * 
+     * @param array
+     * @return element
+     */
+    public static int mostPopular(int[] array)
+    {
         int element=0;
         int count = 0;
-        
-        System.out.println("This program takes integer input from the user and "
-                + "displays which integer is most frequent.\n" );
-        
-            for(int i = 0; i<array.length; i++)
+        //loop that will take one element at a time
+        for(int i = 0; i<array.length; i++)
+        {
+            int currentCount = 0;
+            int currentElement = array[i];
+
+            //loop that will compare the currentElement with the subsequent elements
+            for(int j = 1; j<array.length; j++)
             {
-                int currentCount = 0;
-                int currentElement = array[i];
-                
-                for(int j = 1; j<array.length; j++)
+                if(array[j] == currentElement)
+                    currentCount++;
+                if(currentCount > count)
                 {
-                    if(array[j] == currentElement)
-                        currentCount++;
-                    if(currentCount > count)
-                    {
-                        element = currentElement;
-                        count = currentCount;
-                    }
+                    element = currentElement;
+                    count = currentCount;
                 }
-            }       
-        System.out.println("The most frequent integer value is: " + element);
+            }
+        }       
+        return element;
     }
-   
-    
 }
